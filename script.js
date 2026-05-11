d36dc89ba46280121d9e07fe5f5711c9711bdce5
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-11 19:09:38 +0600

    did skills and contact & footers

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Portfolio Loaded");
+function showTab(tabId){
+
+    const tabs =
+    document.querySelectorAll(".tab-content");
+
+    const buttons =
+    document.querySelectorAll(".tab-btn");
+
+    tabs.forEach(tab=>{
+        tab.classList.remove("active-tab");
+    });
+
+    buttons.forEach(btn=>{
+        btn.classList.remove("active");
+    });
+
+    document
+    .getElementById(tabId)
+    .classList.add("active-tab");
+
+    event.target.classList.add("active");
+}
